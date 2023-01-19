1bdeff457b494c520c912db50de6cc851d22c6ad
unknown <deliah07@naver.com>
2023-01-19 15:13:07 +0900

    로그아웃 기능 완료

--- src/main/java/shop/mtcoding/buyer/controller/userController.java
+++ src/main/java/shop/mtcoding/buyer/controller/userController.java
@@ -1,5 +1,8 @@
 package shop.mtcoding.buyer.controller;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,16 +38,40 @@ public class userController {
     }
 
     @GetMapping("/loginForm")
-    public String loginForm() {
+    public String loginForm(HttpServletRequest request) {
+        String username = "";
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("remember")) {
+                username = cookie.getValue();
+            }
+        }
+        request.setAttribute("remember", username);
+        System.out.println("디버그 : " + cookies);
         return "user/loginForm";
     }
 
     @PostMapping("/login")
-    public String login(String username, String password) {
+    public String login(String username, String password, String remember, HttpServletResponse response) {
         User user = UserRepository.findByUsernameAndPassword(username, password);
         if (user == null) {
-            return "user/loginFrom";
+            return "redirect:/loginForm";
         } else {
+            // 요청헤더 : cookie
+            // 응답헤더 : Set-Cookie
+            if (remember == null) {
+                remember = "";
+            }
+
+            if (remember.equals("on")) {
+                Cookie cookie = new Cookie("remember", username);
+                response.addCookie(cookie);
+            } else {
+                Cookie cookie = new Cookie("remember", "");
+                cookie.setMaxAge(0);
+                response.addCookie(cookie);
+            }
+
             session.setAttribute("principal", user);
             return "redirect:/";
         }
